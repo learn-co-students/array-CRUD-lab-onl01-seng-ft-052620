bd66fc26608e9b83ddb5170addf302fb3cedd6e0
Georgette Class-Peters <georgycp@udel.edu>
2020-06-04 17:22:11 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,94 +1,35 @@
  def create_an_empty_array
-[ ]
+[]
  end 
  
  def create_an_array
- array= ["wow", "I", "am","really"]
+  Array.new(4)
  end
  
- def add_element_to_end_of_array(array, element)
-  array.push(element)
+ def add_element_to_end_of_array(array,element)
+   array.push("arrays!")
  end
  
- def add_element_to_start_of_array
- end
- 
- def remove_element_from_end_of_array
+ def add_element_to_start_of_array(array, element)
+   array.unshift("wow")
  end 
  
- def remove_element_from_start_of_array
+ def remove_element_from_end_of_array(array)
+   array.pop
  end 
-# require "spec_helper"
+ 
+  def remove_element_from_start_of_array(array)
+    array.shift
+ end 
+ 
+ def retrieve_element_from_index(array, index_number)
+   array[2]
+ end
+ 
+ def retrieve_first_element_from_array(array)
+  array.first
+ end 
 
-# describe "#create_an_empty_array" do
-#   it "creates and returns and empty array" do 
-#     expect(create_an_empty_array).to eq([])
-#   end 
-# end
-
-# describe "create_an_array" do 
-#   it "creates an array with four elements" do 
-#     expect(create_an_array).to be_a(Array)
-#     expect(create_an_array.size).to eq(4)
-#   end
-# end
-
-# describe "add_element_to_end_of_array" do 
-#   it "takes arguments of an array and an element and adds that element to the end of the array" do 
-#     array = ["wow", "I", "am", "really", "learning"]
-#     element = "arrays!"
-#     expect(add_element_to_end_of_array(array, element).last).to eq("arrays!")
-#   end
-# end
-
-# describe "add_element_to_start_of_array" do 
-#   it "takes arguments of an array and an element and adds that element to the start of the array" do 
-#     array = ["I", "am", "really", "learning"]
-#     element = "wow"
-#     expect(add_element_to_start_of_array(array, element).first).to eq("wow")
-#   end
-# end
-
-# describe "remove_element_from_end_of_array" do 
-#   it "takes in an argument of an array and removes the last element in the array" do 
-#     array = ["I", "am", "really", "learning", "arrays!"]
-#     expect(remove_element_from_end_of_array(array)).to eq("arrays!")
-#   end
-# end
-
-# describe "remove_element_from_start_of_array" do 
-#   it "takes in an argument of an array and removes the first element from the array" do 
-#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
-#     expect(remove_element_from_start_of_array(array)).to eq("wow")
-#   end
-# end
-
-# describe "retrieve_element_from_index" do 
-#   it "takes in an argument of an array and an index number and returns the element stored at that index" do 
-#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
-#     index_number = 2
-#     expect(retrieve_element_from_index(array, 2)).to eq("am")
-#   end
-# end
-
-# describe "retrieve_first_element_from_array" do 
-#   it "takes in an argument of an array and returns the first element stored in the array" do 
-#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
-#     expect(retrieve_first_element_from_array(array)).to eq("wow")
-#   end
-# end
-
-# describe "retrieve_last_element_from_array" do 
-#   it "takes in an argument of an array and returns the last element of that array" do 
-#     array = ["wow", "I", "am", "really", "learning", "arrays!"]
-#     expect(retrieve_last_element_from_array(array)).to eq("arrays!")
-#   end
-# end
-
-
-
-
-
-
-
-
+def retrieve_last_element_from_array(array)
+ array[-1]
+end 
